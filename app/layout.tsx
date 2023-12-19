95df674c881b46d0afc1a73372537a00ac6d3ce8
Suat-17 <yargicisuat@gmail.com>
2023-12-19 16:51:35 +0300

    chore: last change

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        <StarsCanvas />
         <Navbar />
         {children}
         <Footer/>

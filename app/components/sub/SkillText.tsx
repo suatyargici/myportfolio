e2f47a4355a9cf64f4011327b580e2f98799b493
Suat-17 <yargicisuat@gmail.com>
2023-12-19 17:24:56 +0300

    chore: last change

--- app/components/sub/SkillText.tsx
+++ app/components/sub/SkillText.tsx
@@ -11,8 +11,8 @@ const SkillText = () => {
           variants={slideInFromTop}
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
+          <SparklesIcon className="md:text-[#b49bff] text-white  mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text md:text-[#b49bff] text-white text-[13px]">
           MY SKILLS
           </h1>
         </motion.div>
@@ -20,7 +20,7 @@ const SkillText = () => {
         variants={slideInFromLeft(0.5)}
         className='text-[30px] text-white font-medium mt-[10px] text-center mb-[15px]'
         >
-            I practice with modern technologies
+            I using modern technologies
         </motion.div>
      
     </div>

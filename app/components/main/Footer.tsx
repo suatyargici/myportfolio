b659072d0ef876e821d5789dbdeacba19405aaa4
Suat-17 <yargicisuat@gmail.com>
2024-01-09 23:52:05 +0300

    chore: current year edited

--- app/components/main/Footer.tsx
+++ app/components/main/Footer.tsx
@@ -10,9 +10,10 @@ import {
 import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <div className="mb-[20px] text-white text-center text-[15px]">
-      &copy; SUAT Dev 2023 Inc. All rights reserved 
+      &copy; SUAT Dev {year} Inc. All rights reserved 
     </div>
   );
 };
